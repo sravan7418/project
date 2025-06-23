38c7f3d44ca6e2536d22f7cc9d043f5565658d27
sravan <sravankumarsalendra036@gmail.com>
2025-06-23 15:43:58 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,8 +1,8 @@
-public class sample
+public class Sample
 {
-	public static void main(String[] args)
-	{
-		System.out.print("sravan kumar");
-	}
+    public static void main(String[] args)
+    {
+        System.out.print("sravan kumar");
+    }
 }
 
